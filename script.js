9af34dc27caad1cd0f1a942bc862dabd4ebfe67c
Rexedge <ap.oyeniran@gmail.com>
2023-06-11 23:13:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+     theme: {
+          extend: {
+               colors: {
+                    clifford: '#da373d',
+               }
+          }
+     }
+}
+
+window.addEventListener('scroll', function () {
+     const navbar = document.getElementById('navbar');
+     const scrolled = window.pageYOffset || document.documentElement.scrollTop;
+
+     if (scrolled > 10) {
+          navbar.classList.add('bg-white', 'backdrop-blur-sm');
+     } else {
+          navbar.classList.remove('bg-white', 'backdrop-blur-sm');
+     }
+})
